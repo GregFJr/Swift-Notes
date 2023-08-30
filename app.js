968e9d99b3823b8e2f8b57c446065cd392791c43
GregFJr <finney101690@gmail.com>
2023-08-30 14:37:36 -0700

    folder structure setup

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFIle(__dirname + '/views/landing.html');
+});
