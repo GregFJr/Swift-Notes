4572ec22f1dbb08f71d4c5fc731c1c798ec36a0c
GregFJr <finney101690@gmail.com>
2023-09-03 11:28:51 -0700

    updated add.js

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+
+let notes = [];
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'landing.html'));
@@ -13,6 +16,15 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'notes.html'));
 });
+
+app.post('/add', (req, res) => {
+    const { title, note } = req.body;
+    notes.push({ title, note });
+
+    console.log(req.body);
+    res.redirect('/');
+});
+
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
