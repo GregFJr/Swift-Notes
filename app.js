74ad9bb69c54bad5ca44d27283d6ac28fdfac0ac
GregFJr <finney101690@gmail.com>
2023-09-16 15:09:10 -0700

    fixed port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT||3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
